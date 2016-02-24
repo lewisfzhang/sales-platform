dcd51e4ba4174e75732ea814fd69866d53f69dca
Kevin Gottlieb <kgred9@gmail.com>
2016-02-24 00:53:53 -0800

    Added the php to send email to everyone who hasn't entered a quotation yet. Not quite done.

---
+++ reminderEmailer.php
@@ -0,0 +1,86 @@
+<?php
+    error_reporting(E_ERROR | E_WARNING | E_PARSE); //doesn't report small errors
+    require('PHPMailer/PHPMailerAutoload.php'); //PHPMailer file
+    $db = new SQLite3('quotations2016.sqlite3'); //connect
+
+    function sendMail($to, $subject, $message){ //send email
+        $mail = new PHPMailer;
+        $mail->isSMTP();
+        $mail->Host = 'localhost';
+        $mail->Port = 25;
+
+        //Set initial mail headers
+        $mail->From = "carillon@bcp.org";
+        $mail->FromName = "The Carillon";
+        $mail->AddBCC('carillon@bcp.org');
+        $mail->AddAddress($to);
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+        $mail->IsHTML(true);
+                
+        return $mail->send(); //will return true if sending worked
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Reminder Emailer</title>
+        <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css"> <!--W3.CSS stylesheet-->
+    </head>
+    <body>
+        <form class="w3-container" method="post">
+            <input type="submit" name="sendEmail" value="Send Email to Seniors who have not Submitted" class="w3-btn w3-theme">
+        </form>
+        <?php
+            if(isset($_POST['sendEmail'])){
+                $statement = $db -> prepare('SELECT * FROM quotations'); 
+                $result = $statement->execute();
+
+                //create an array for all of the column values
+                $emailArray[] = [];
+                $firstNameArray[] = [];
+                $urlArray[] = [];
+                $quotationArray[] = [];
+                $processedStudentArray[] = [];
+                $processedTeacherArray[] =[];
+
+                while($row = $result->fetchArray(SQLITE3_ASSOC)){
+                    array_push($emailArray, $row['email']); //set the values in to the array
+                }
+                while($row = $result->fetchArray(SQLITE3_ASSOC)){
+                    array_push($firstNameArray, $row['firstName']); //set the values in to the array
+                }
+                while($row = $result->fetchArray(SQLITE3_ASSOC)){
+                    array_push($urlArray, $row['url']); //set the values in to the array
+                }
+                while($row = $result->fetchArray(SQLITE3_ASSOC)){
+                    array_push($quotationArray, $row['quotation']); //set the values in to the array
+                }
+                while($row = $result->fetchArray(SQLITE3_ASSOC)){
+                    array_push($processedStudentArray, $row['processedStudent']); //set the values in to the array
+                }
+                while($row = $result->fetchArray(SQLITE3_ASSOC)){
+                    array_push($processedTeacherArray, $row['processedTeacher']); //set the values in to the array
+                }
+
+                $index = 0; //the current index the emailer is on
+                foreach($emailArray as $email){
+                    $quotation = $quotationArray[$index];
+                    $firstName = $firstNameArray[$index];
+                    $url = $urlArray[$index];
+                    $processedStudent = $processedStudentArray[$index];
+                    $processedTeacher = $processedTeacherArray[$index];
+
+                    //send an email if student hasn't entered quotation yet
+                    if(!isset($quotation) or $quotation == NULL or $quotation == "" or $processedStudent == -1 or $processedTeacher == -1){
+                        
+                    } 
+
+                    $index = $index + 1; //increment index
+                }
+            }
+        ?>
+    </body>
+</html>
